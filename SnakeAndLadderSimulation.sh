cb5f42652fdb9f9821a6c24711ddcbca19f60234
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-05 15:34:05 +0530

    ADDED: Options for player to choose the next move

--- SnakeAndLadderSimulation.sh
+++ SnakeAndLadderSimulation.sh
@@ -1,9 +1,31 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "Welcome to Snake And Ladder simulation"
 
 #CONSTANT
 PLAYER_START_POSITION=0
+NO_PLAY=1
+LADDER=2
+SNAKE=3
 
 #VARIABLE
-random=$((RANDOM%6+1))
+dieValue=$((RANDOM%6+1))
+playerMove=$((RANDOM%3+1))
+playerCurrentPosition=$PLAYER_START_POSITION
 
+	#Checking the options for player's next move
+function playerNextMove()
+{
+	case $playerMove in
+			$NO_PLAY)
+					playerCurrentPosition=$playerCurrentPosition
+					;;
+			$LADDER)
+					playerCurrentPosition=$(( playerCurrentPosition + dieValue ))
+					;;
+			$SNAKE)
+					playerCurrentPosition=$(( playerCurrentPosition - dieValue ))
+					;;
+	esac
+}
+
+playerNextMove

28c984319e8e94335104fc98c7cf2c3ce852bac8
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-07 12:12:16 +0530

    ADDED: Created a 2 player game and reporting the winner

--- SnakeAndLadderSimulation.sh
+++ SnakeAndLadderSimulation.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 echo "Welcome to Snake And Ladder simulation"
-declare -A diceAndPosition
 
 #CONSTANTS
-PLAYER_START_POSITION=0
+PLAYER_STARTING_POSITION=0
 PLAYER_WINNING_POSITION=100
 NO_PLAY=1
 LADDER=2
 SNAKE=3
 
-#VARIABLE
-playerCurrentPosition=$PLAYER_START_POSITION
-dice=0
+#VARIABLES
+playerCurrentPosition=$PLAYER_STARTING_POSITION
+positionOfPlayer1=$PLAYER_STARTING_POSITION
+positionOfPlayer2=$PLAYER_STARTING_POSITION
 
 #Checking the options for player's next move
 function playerNextMove()
 {
-	randomValues
+	playerCurrentPosition=$1
+	generatingRandomValues
 	case $playerMove in
 		$NO_PLAY)
 			playerCurrentPosition=$playerCurrentPosition
@@ -28,22 +29,28 @@ function playerNextMove()
 			snakeMoves
 			;;
 	esac
+	#If either of the player reaches the 100th position first then that player wins and the game's stopped
+	if (( $playerCurrentPosition == 100 ))
+	then
+		echo "$2 wins"
+		exit
+	fi
 }
 
-#Function to Generate random values
-function randomValues()
+#Function to Generate random values for die roll and player's next move
+function generatingRandomValues()
 {
-	playerMove=$((RANDOM%3+1))
-	dieValue=$((RANDOM%6+1))
+	playerMove=$(( RANDOM%3+1 ))
+	dieValue=$(( RANDOM%6+1 ))
 }
 
 #Function to perform the ladder's operation
 function ladderMoves()
 {
 	playerCurrentPosition=$(( $playerCurrentPosition + $dieValue ))
-	if (( $playerCurrentPosition > $PLAYER_WINNING_POSITION))
+	if (( $playerCurrentPosition > $PLAYER_WINNING_POSITION ))
 	then
-		playerCurrentPosition=$(($playerCurrentPosition - $dieValue))
+		playerCurrentPosition=$(( $playerCurrentPosition - $dieValue ))
 	fi
 }
 
@@ -51,29 +58,25 @@ function ladderMoves()
 function snakeMoves()
 {
 	playerCurrentPosition=$(( $playerCurrentPosition - $dieValue ))
-	if (( $playerCurrentPosition < $PLAYER_START_POSITION))
+	if (( $playerCurrentPosition < $PLAYER_STARTING_POSITION))
 	then
-		playerCurrentPosition=$PLAYER_START_POSITION
+		playerCurrentPosition=$PLAYER_STARTING_POSITION
 	fi
 }
 
-#Function to store the dice count and the player's position at that time
-function diceAndPosition()
-{
-	((dice++))
-	diceAndPosition[dice-$dice]="$playerCurrentPosition"
-}
-
-#Function to play till the player wins
+#Function to play till one of  the player wins
 function play()
 {
-	while (( $playerCurrentPosition < $PLAYER_WINNING_POSITION ))
+	while (( $positionOfPlayer1 < $PLAYER_WINNING_POSITION && $positionOfPlayer2 < $PLAYER_WINNING_POSITION ))
 	do
-		playerNextMove
-		diceAndPosition
+		playerNextMove $positionOfPlayer1 Player1
+		positionOfPlayer1=$playerCurrentPosition
+
+		playerNextMove $positionOfPlayer2 Player2
+		positionOfPlayer2=$playerCurrentPosition
 	done
 }
 
-
 #MAIN
+
 play
